0491d9a957555909cfe3aa797b78ef524a5a012d
VuksanFilip <filipvuksan.01@gmail.com>
2024-05-17 11:09:53 +0200

    Added Album endpoints

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/AlbumController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/AlbumController.java
@@ -1,11 +1,33 @@
 package com.ftn.sbnz.service.controllers;
 
+import com.ftn.sbnz.model.models.dtos.AlbumDTO;
+import com.ftn.sbnz.model.models.dtos.SongDTO;
+import com.ftn.sbnz.service.services.AlbumService;
+import com.ftn.sbnz.service.services.ArtistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/albums")
 public class AlbumController {
+
+    private final AlbumService albumService;
+
+    @GetMapping(value = "/by-song/{songId}")
+    public ResponseEntity<AlbumDTO> getAlbumBySong(@PathVariable("songId") Long songId){
+        return new ResponseEntity<>(albumService.findBySong(songId), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/by-artist/{artistId}")
+    public ResponseEntity<AlbumDTO> getAlbumByArtist(@PathVariable("artistId") Long artistId){
+        return new ResponseEntity<>(albumService.findByArtist(artistId), HttpStatus.OK);
+    }
 }
--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/AlbumService.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/AlbumService.java
@@ -1,4 +1,9 @@
 package com.ftn.sbnz.service.services;
 
+import com.ftn.sbnz.model.models.dtos.AlbumDTO;
+
 public interface AlbumService {
+    AlbumDTO findBySong(Long songId);
+
+    AlbumDTO findByArtist(Long artistId);
 }
