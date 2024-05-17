2269cf2c313eea612c9e184bbc1f09642dfb73c2
popovbojana <popov.bojana@gmail.com>
2024-05-17 11:04:55 +0200

    Update SongService.java

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/SongService.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/SongService.java
@@ -1,14 +1,13 @@
 package com.ftn.sbnz.service.services;
 
 import com.ftn.sbnz.model.models.Song;
-import com.ftn.sbnz.service.dtos.RatingDTO;
 
 public interface SongService {
 
     Song findById(Long id);
 
-//    String addToFavoriteSongs(Long userId, Long songId);
-//
-//    String addToListenedSongs(Long userId, Long songId);
+    String addToFavoriteSongs(Long userId, Long songId);
+
+    String addToListenedSongs(Long userId, Long songId);
 
 }
