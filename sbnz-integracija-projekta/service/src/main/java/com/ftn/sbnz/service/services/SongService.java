5f3e9048a22c838db623db9640574371d400baa6
popovbojana <popov.bojana@gmail.com>
2024-05-17 11:06:50 +0200

    Small refactor

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/SongService.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/SongService.java
@@ -1,6 +1,9 @@
 package com.ftn.sbnz.service.services;
 
 import com.ftn.sbnz.model.models.Song;
+import com.ftn.sbnz.model.models.dtos.SongDTO;
+
+import java.util.List;
 
 public interface SongService {
 
@@ -9,5 +12,11 @@ public interface SongService {
     String addToFavoriteSongs(Long userId, Long songId);
 
     String addToListenedSongs(Long userId, Long songId);
+
+    List<SongDTO> findAll();
+
+    List<SongDTO> findAllByArtist(Long artistId);
+
+    List<SongDTO> findAllByGenre(Long genreId);
 
 }
